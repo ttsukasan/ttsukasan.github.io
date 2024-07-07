403c5586cd91270081e35e133d69fe1c3fe299dc
ttsuka <tmak.tsukamoto@gmail.com>
2024-07-07 21:12:36 +0900

    daisyuiを使ったレイアウト

--- js/app.ts
+++ js/app.ts
@@ -2,3 +2,8 @@ import '../css/style.css';
 
 const welcome: string = 'typescript';
 console.log(welcome);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("year");
+  yearSpan.textContent = new Date().getFullYear().toString();
+});
